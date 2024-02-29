3956fc3fbece7554e34a1998406414a8ff3ef08d
TMTABC <tmtri22052004@gmail.com>
2024-02-29 18:08:06 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 const app = require("./app");
 const config = require("./app/config");
+const MongoDB =require("./app/utils/mongodb.util");
 
-const PORT = config.app.port;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}.`);
-})
+
+
+const startServer= async()=>{
+    try{
+        await MongoDB.connect(config.db.url);
+        console.log("Connect to the database!!!");
+
+        const PORT = config.app.port;
+        app.listen(PORT,()=>{
+            console.log(`Server is running on port ${PORT}.`);
+        })
+    }catch(err){
+        console.log("Cannot connect to the database!",err);
+        process.exit();
+    }
+}
+
+startServer();
